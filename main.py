bfac43adb59c33868d19ccbac8dcf65560768126
Tim K. Chan <timchan@berkeley.edu>
2017-01-31 16:55:09 +0000

    MQTT broadcast working

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import machine
+import network
 import time
+from umqtt.simple import MQTTClient
 
 print('main.py loading...')
 
@@ -29,7 +31,44 @@ print('main.py loaded successfully')
 print('hint:')
 print('Try dieTemp() or objTemp()')
 
+# Function to print out objTemp at a certain time (ms) interval.
 def objTempRoll(i):
 	for __ in range(i):
 		time.sleep_ms(300)
 		print(objTemp())
+
+# Function to conenct to the wifi.
+def do_connect():
+    
+    SSID = 'MotoG3'
+    PASSWORD = 'embedded'
+
+    sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
+    if ap_if.active():
+        ap_if.active(False)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(SSID, PASSWORD)
+        while not sta_if.isconnected():
+            pass
+    print('Network configuration:', sta_if.ifconfig())
+
+do_connect()
+
+def publish(message, server="broker.hivemq.com"):
+    c = MQTTClient("clientId-UADVnHCKdM", server)
+    c.connect()
+    c.publish(b"practiceReminder", message)
+    c.disconnect()
+
+def publishTemp(i):
+	for __ in range(i):
+		time.sleep(3)
+		publish(str(objTemp()))
+
+
+
+
+
